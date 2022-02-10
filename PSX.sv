4d9a97aa8b41431a92075e372a3572fbc22840ce
Nolan Nicholson <nolananicholson@gmail.com>
2022-02-09 21:00:23 -0800

    Initial Namco GunCon support

--- PSX.sv
+++ PSX.sv
@@ -252,8 +252,8 @@ parameter CONF_STR = {
 	"-;",
 	"o78,System Type,NTSC-U,NTSC-J,PAL;",
    "-;",
-	"oDF,Pad1,Digital,Analog,Mouse,Off;",
-	"oGI,Pad2,Digital,Analog,Mouse,Off;",
+	"oDF,Pad1,Digital,Analog,Mouse,Off,GunCon;",
+	"oGI,Pad2,Digital,Analog,Mouse,Off,GunCon;",
    "-;",
 	"OG,Fastboot,Off,On;",
 	"d1oC,SPU RAM select,DDR3,SDRAM2;",
@@ -584,14 +584,17 @@ defparam savestate_ui.INFO_TIMEOUT_BITS = 27;
 // 000 -> analog
 // 000 -> mouse
 // 011 -> off
-// 100..111 -> reserved
+// 100 -> Namco GunCon lightgun
+// 101..111 -> reserved
 
 wire PadPortEnable1 = (status[47:45] != 3'b011);
 wire PadPortAnalog1 = (status[47:45] == 3'b001);
 wire PadPortMouse1  = (status[47:45] == 3'b010);
+wire PadPortGunCon1 = (status[47:45] == 3'b100);
 wire PadPortEnable2 = (status[50:48] != 3'b011);
 wire PadPortAnalog2 = (status[50:48] == 3'b001);
 wire PadPortMouse2  = (status[50:48] == 3'b010);
+wire PadPortGunCon2 = (status[50:48] == 3'b100);
 
 ////////////////////////////  SYSTEM  ///////////////////////////////////
 
@@ -708,9 +711,11 @@ psx
    .PadPortEnable1 (PadPortEnable1),
    .PadPortAnalog1 (PadPortAnalog1),
    .PadPortMouse1  (PadPortMouse1 ),
+   .PadPortGunCon1 (PadPortGunCon1),
    .PadPortEnable2 (PadPortEnable2),
    .PadPortAnalog2 (PadPortAnalog2),
    .PadPortMouse2  (PadPortMouse2 ),
+   .PadPortGunCon2 (PadPortGunCon2),
    .KeyTriangle({joy2[4], joy[4] }),    
    .KeyCircle  ({joy2[5] ,joy[5] }),       
    .KeyCross   ({joy2[6] ,joy[6] }),       
